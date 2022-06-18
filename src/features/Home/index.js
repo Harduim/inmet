c2dde56495c59f8b9315607c6f45ecaf521d4bf0
Arthur Harduim <arthur.harduim@rioenergy.com.br>
2022-06-17 21:18:10 -0300

    Chore: remove example

--- src/features/Home/index.js
+++ src/features/Home/index.js
@@ -1,16 +1,9 @@
 import PageFrame from '../shared/PageFrame'
-import { useContext } from 'react'
-import EstacaoContext from '../../context/EstacaoContext'
 
 const Home = () => {
-  const { estacoes, estacao, estacoesIsLoading } = useContext(EstacaoContext)
   return (
     <PageFrame>
-      <div>TESTE</div>
-      <div>{JSON.stringify(estacao)}</div>
-      <div>{JSON.stringify(estacoesIsLoading)}</div>
-      <div>OI</div>
-      <div>{JSON.stringify(estacoes)}</div>
+      <div>Home</div>
     </PageFrame>
   )
 }
